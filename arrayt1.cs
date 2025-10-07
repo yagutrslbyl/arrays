6610f020848e17662f46ef8a0035f2e649bad3c5
yagutrslbyl <yaguter-bpa202@code.edu.az>
2025-10-08 03:10:50 +0400

    Task1

---
+++ arrayt1.cs
@@ -0,0 +1,14 @@
+﻿// ededin mertebelerinin en boyuyu
+Console.WriteLine("Enter a number:");
+int n = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+while (n > 0)
+{
+    n = n % 10;
+    if (max < n)
+    {
+        max = n;
+    }
+    n = n / 10;
+}
+Console.WriteLine(max);
