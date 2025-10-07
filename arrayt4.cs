6ed760e8955b04f7b6b25f2cb7bace30f7a8e3a2
yagutrslbyl <yaguter-bpa202@code.edu.az>
2025-10-08 03:55:12 +0400

    Task4

---
+++ arrayt4.cs
@@ -0,0 +1,11 @@
+﻿// hem cut hem 3e bol sayi
+int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 33 };
+int count = 0;
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] % 2 == 0 && numbers[i]%3==0)
+    {
+        count = count + 1;
+    }
+}
+Console.WriteLine(count);
