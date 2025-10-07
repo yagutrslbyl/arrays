2784c7054c1967f40a504ce692df52b82a9da91f
yagutrslbyl <yaguter-bpa202@code.edu.az>
2025-10-08 03:49:05 +0400

    Task3

---
+++ arrayt3.cs
@@ -0,0 +1,13 @@
+﻿//en kicik ve en b cemi
+int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 7 };
+int max = numbers[0];
+int min = numbers[0];
+for (int i = 1; i < numbers.Length; i++)
+{
+    if (max < numbers[i])
+        max = numbers[i];
+    if (min > numbers[i])
+        min = numbers[i];
+}
+int sum = max + min;
+Console.WriteLine(sum);
