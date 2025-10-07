3efd0af286597fe5b74c7663b313878b3a799a8f
yagutrslbyl <yaguter-bpa202@code.edu.az>
2025-10-08 03:42:14 +0400

    Task2

---
+++ arrayt2.cs
@@ -0,0 +1,22 @@
+﻿// eded ikinin quvvetidir ya yox
+Console.WriteLine("eded daxil et");
+int n = Convert.ToInt32(Console.ReadLine());
+bool quvvet = false;
+if (n > 0)
+{
+    while (n % 2 == 0)
+
+    {
+        n = n / 2;
+    }
+    if (n == 1)
+    {
+        quvvet = true;
+    }
+    if (quvvet)
+        Console.WriteLine("Eded ikinin quvvetidir");
+    else
+        Console.WriteLine("Eded ikinin quvvet deyil");
+}
+else
+    Console.WriteLine("musbet eded daxil edin");
