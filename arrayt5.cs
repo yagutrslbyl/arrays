05d4015652b6ca44c3578a88c7ef9c47495f7bb8
yagutrslbyl <yaguter-bpa202@code.edu.az>
2025-10-08 04:10:47 +0400

    Task5

---
+++ arrayt5.cs
@@ -0,0 +1,15 @@
+﻿// murekkeb sayi
+int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 33 };
+int count = 0;
+for(int i=0;i<numbers.Length;i++)
+{
+    for (int j = 2; j < numbers[i]; j++)
+    {
+        if (numbers[i]>3 && numbers[i] % j == 0)
+            count += 1;
+            break;
+
+    }
+    
+}
+Console.WriteLine(count);
